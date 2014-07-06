2d42ae17da387230fcd8025bee0e086052d0f8bd
Jake Craige <jake@poeticsystems.com>
2014-07-06 13:45:24 -0500

    initial commit loading QUnit, no image assets for notifications yet

---
+++ README.md
@@ -0,0 +1,15 @@
+## ember-cli-qunit
+
+This addon that adds `QUnit` to the generated Ember CLI test output (in `test-vendor.js`).
+
+### Installation / Usage
+
+From within your Ember CLI application (must be > 0.0.39), run the following:
+
+```bash
+npm install --save-dev ember-cli-qunit
+```
+
+### References
+
+* [qunit](https://github.com/jquery/qunit)
---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var path      = require('path');
+var fs        = require('fs');
+
+function EmberCLIQUnit(project) {
+  this.project = project;
+  this.name    = 'Ember CLI QUnit';
+}
+
+function unwatchedTree(dir) {
+  return {
+    read:    function() { return dir; },
+    cleanup: function() { }
+  };
+}
+
+
+EmberCLIQUnit.prototype.treeFor = function treeFor(name) {
+  if(name !== 'vendor') { return; }
+
+  var treePath = path.join(__dirname, 'vendor');
+
+  if (fs.existsSync(treePath)) {
+    return treePath;
+  }
+};
+
+EmberCLIQUnit.prototype.included = function included(app) {
+  this.app = app;
+
+  if (this.app.tests) {
+    this.app.import('vendor/qunit/qunit/qunit.js', {
+      type: 'test'
+    });
+    this.app.import('vendor/qunit/qunit/qunit.css', {
+      type: 'test'
+    });
+    this.app.import('vendor/qunit-notifications/index.js', {
+      type: 'test'
+    });
+    this.app.import('vendor/ember-cli-test-loader/test-loader.js', {
+      type: 'test'
+    });
+  }
+};
+
+module.exports = EmberCLIQUnit;
