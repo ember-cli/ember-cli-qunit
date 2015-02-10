efd786834b766ed7c528c40355fddf7c2dbc4340
Robert Jackson <robert.w.jackson@me.com>
2015-02-10 13:34:01 -0500

    Prevents module load errors from stopping test run.

--- blueprints/ember-cli-qunit/index.js
+++ blueprints/ember-cli-qunit/index.js
@@ -8,7 +8,7 @@ module.exports = {
   afterInstall: function() {
     return this.addBowerPackagesToProject([
       { name: 'qunit',                           target: '~1.17.0' },
-      { name: 'ember-cli/ember-cli-test-loader', target: '0.1.0'   },
+      { name: 'ember-cli/ember-cli-test-loader', target: '0.1.1'   },
       { name: 'ember-qunit-notifications',       target: '0.0.5'   },
       { name: 'ember-qunit',                     target: '0.2.6' }
     ]);
