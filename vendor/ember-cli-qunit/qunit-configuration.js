9f1085ca13f1a2d939a348cf20e4412b1803e495
Taylor Hobbs <taylor@feemon.com>
2015-03-24 12:01:12 -0500

    Feature: Allow testing preview container position to be toggleable

--- vendor/ember-cli-qunit/qunit-configuration.js
+++ vendor/ember-cli-qunit/qunit-configuration.js
@@ -1,7 +1,8 @@
 /* globals jQuery,QUnit */
 
-QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container' });
+QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container'});
 QUnit.config.urlConfig.push({ id: 'nojshint', label: 'Disable JSHint'});
+QUnit.config.urlConfig.push({ id: 'doccontainer', label: 'Doc test pane'});
 
 if (QUnit.notifications) {
   QUnit.notifications({
@@ -14,5 +15,7 @@ if (QUnit.notifications) {
 
 jQuery(document).ready(function() {
   var containerVisibility = QUnit.urlParams.nocontainer ? 'hidden' : 'visible';
+  var containerPosition = QUnit.urlParams.doccontainer ? 'absolute' : 'relative';
   document.getElementById('ember-testing-container').style.visibility = containerVisibility;
+  document.getElementById('ember-testing-container').style.position = containerPosition;
 });
