92e08232403d7afbf0d7f1e2672d02e72bfb5958
Robert Jackson <robert.w.jackson@me.com>
2015-07-31 15:52:59 -0400

    Update QUnit version to 1.18.0.
    [Changelog](https://github.com/jquery/qunit/blob/1.18.0/History.md):
    
    * Assert: throws uses push method only
    * Assert: Fix missing test on exported throws
    * Assert: Implements notOk to assert falsy values
    * Core: More graceful handling of AMD
    * Core: Simplify stack trace methods
    * Core: Expose Dump maxDepth property
    * Core: Expose QUnit version as QUnit.version property
    * Core: Handle multiple testId parameters
    * Dump: Fix .name/.property doublettes
    * HTML Reporter: New diff using Google's Diff-Patch-Match Library
    * HTML Reporter: Make it more obvious why diff is suppressed.
    * HTML Reporter: Change display text for bad tests
    * HTML Reporter: Fix checkbox and select handling in IE <9
    * HTML Reporter: Fix test filter without any module
    * HTML Reporter: Retain failed tests numbers
    * Test: lowercase the valid test filter before using it

--- blueprints/ember-cli-qunit/index.js
+++ blueprints/ember-cli-qunit/index.js
@@ -7,7 +7,7 @@ module.exports = {
 
   afterInstall: function() {
     return this.addBowerPackagesToProject([
-      { name: 'qunit',                           target: '~1.17.1' },
+      { name: 'qunit',                           target: '~1.18.0' },
       { name: 'ember-cli/ember-cli-test-loader', target: '0.1.3'   },
       { name: 'ember-qunit-notifications',       target: '0.0.7'   },
       { name: 'ember-qunit',                     target: '0.4.9'   }
