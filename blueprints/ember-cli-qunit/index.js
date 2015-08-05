deaf002322597fd044562f0e5c7674c9aa5c4c76
Ben Limmer <ben@benlimmer.com>
2015-08-05 11:10:26 -0600

    Fix default blueprint by specifying source. Fixes #72

--- blueprints/ember-cli-qunit/index.js
+++ blueprints/ember-cli-qunit/index.js
@@ -8,7 +8,7 @@ module.exports = {
   afterInstall: function() {
     return this.addBowerPackagesToProject([
       { name: 'qunit',                           target: '~1.18.0' },
-      { name: 'ember-cli/ember-cli-test-loader', target: '0.1.3'   },
+      { name: 'ember-cli-test-loader',           target: '0.1.3'   },
       { name: 'ember-qunit-notifications',       target: '0.0.7'   },
       { name: 'ember-qunit',                     target: '0.4.9'   }
     ]);
