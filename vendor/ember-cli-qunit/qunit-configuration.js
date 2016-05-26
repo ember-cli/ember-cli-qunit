2e2d0632dcf9ff94b5e7b9788a2e5efef19e61c5
Trent Willis <twillis@linkedin.com>
2016-05-26 13:29:22 -0700

    Introduce development mode option
    Adds a new config option to enable a "development" mode that includes
    a full-screen takeover for the container and no timeout limit

--- vendor/ember-cli-qunit/qunit-configuration.js
+++ vendor/ember-cli-qunit/qunit-configuration.js
@@ -3,7 +3,9 @@
 QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container'});
 QUnit.config.urlConfig.push({ id: 'nolint', label: 'Disable Linting'});
 QUnit.config.urlConfig.push({ id: 'dockcontainer', label: 'Dock container'});
-QUnit.config.testTimeout = 60000; //Default Test Timeout 60 Seconds
+QUnit.config.urlConfig.push({ id: 'devmode', label: 'Development mode' });
+
+QUnit.config.testTimeout = QUnit.urlParams.devmode ? null : 60000; //Default Test Timeout 60 Seconds
 
 if (QUnit.notifications) {
   QUnit.notifications({
@@ -18,8 +20,15 @@ jQuery(document).ready(function() {
   var testContainer = document.getElementById('ember-testing-container');
   if (!testContainer) { return; }
 
-  var containerVisibility = QUnit.urlParams.nocontainer ? 'hidden' : 'visible';
-  var containerPosition = QUnit.urlParams.dockcontainer ? 'absolute' : 'relative';
+  var params = QUnit.urlParams;
+
+  var containerVisibility = params.nocontainer ? 'hidden' : 'visible';
+  var containerPosition = (params.dockcontainer || params.devmode) ? 'absolute' : 'relative';
+
+  if (params.devmode) {
+    testContainer.className = ' full-screen';
+  }
+
   testContainer.style.visibility = containerVisibility;
   testContainer.style.position = containerPosition;
 });
