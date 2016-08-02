a5447057cf263804a5c4e8487d975b8a6e15f646
Robert Jackson <rwjblue@twitch.tv>
2016-08-02 17:44:15 -0400

    Remove jQuery requirement.

--- vendor/ember-cli-qunit/test-loader.js
+++ vendor/ember-cli-qunit/test-loader.js
@@ -1,60 +1,71 @@
-/* globals jQuery, QUnit, require, requirejs */
+/* globals QUnit, require, requirejs */
 
-jQuery(document).ready(function() {
-  var testLoaderModulePath = 'ember-cli-test-loader/test-support/index';
-
-  if (!requirejs.entries[testLoaderModulePath]) {
-    testLoaderModulePath = 'ember-cli/test-loader';
-  }
-
-  var TestLoaderModule = require(testLoaderModulePath);
-  var TestLoader = TestLoaderModule['default'];
-  var addModuleExcludeMatcher = TestLoaderModule['addModuleExcludeMatcher'];
-  var addModuleIncludeMatcher = TestLoaderModule['addModuleIncludeMatcher'];
-
-  function excludeModule(moduleName) {
-    return QUnit.urlParams.nolint &&
-           moduleName.match(/\.(jshint|lint-test)$/);
-  }
-
-  function includeModule(moduleName) {
-    return moduleName.match(/\.jshint$/);
-  }
-
-  if (addModuleExcludeMatcher && addModuleIncludeMatcher) {
-    addModuleExcludeMatcher(excludeModule);
-    addModuleIncludeMatcher(includeModule);
-  } else {
-    TestLoader.prototype.shouldLoadModule = function shouldLoadModule(moduleName) {
-      return (moduleName.match(/[-_]test$/) || includeModule(moduleName)) && !excludeModule(moduleName);
-    };
-  }
-
-  var moduleLoadFailures = [];
-
-  TestLoader.prototype.moduleLoadFailure = function(moduleName, error) {
-    moduleLoadFailures.push(error);
-
-    QUnit.module('TestLoader Failures');
-    QUnit.test(moduleName + ': could not be loaded', function() {
-      throw error;
-    });
-  };
-
-  QUnit.done(function() {
-    if (moduleLoadFailures.length) {
-      throw new Error('\n' + moduleLoadFailures.join('\n'));
+(function() {
+  function ready(fn) {
+    if (document.readyState != 'loading'){
+      fn();
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
     }
+  }
+
+  ready(function() {
+    var testLoaderModulePath = 'ember-cli-test-loader/test-support/index';
+
+    if (!requirejs.entries[testLoaderModulePath]) {
+      testLoaderModulePath = 'ember-cli/test-loader';
+    }
+
+    var TestLoaderModule = require(testLoaderModulePath);
+    var TestLoader = TestLoaderModule['default'];
+    var addModuleExcludeMatcher = TestLoaderModule['addModuleExcludeMatcher'];
+    var addModuleIncludeMatcher = TestLoaderModule['addModuleIncludeMatcher'];
+
+    function excludeModule(moduleName) {
+      return QUnit.urlParams.nolint &&
+        moduleName.match(/\.(jshint|lint-test)$/);
+    }
+
+    function includeModule(moduleName) {
+      return moduleName.match(/\.jshint$/);
+    }
+
+    if (addModuleExcludeMatcher && addModuleIncludeMatcher) {
+      addModuleExcludeMatcher(excludeModule);
+      addModuleIncludeMatcher(includeModule);
+    } else {
+      TestLoader.prototype.shouldLoadModule = function shouldLoadModule(moduleName) {
+        return (moduleName.match(/[-_]test$/) || includeModule(moduleName)) && !excludeModule(moduleName);
+      };
+    }
+
+    var moduleLoadFailures = [];
+
+    TestLoader.prototype.moduleLoadFailure = function(moduleName, error) {
+      moduleLoadFailures.push(error);
+
+      QUnit.module('TestLoader Failures');
+      QUnit.test(moduleName + ': could not be loaded', function() {
+        throw error;
+      });
+    };
+
+    QUnit.done(function() {
+      if (moduleLoadFailures.length) {
+        throw new Error('\n' + moduleLoadFailures.join('\n'));
+      }
+    });
+
+    var autostart = QUnit.config.autostart !== false;
+    QUnit.config.autostart = false;
+
+    setTimeout(function() {
+      TestLoader.load();
+
+      if (autostart) {
+        QUnit.start();
+      }
+    }, 250);
   });
 
-  var autostart = QUnit.config.autostart !== false;
-  QUnit.config.autostart = false;
-
-  setTimeout(function() {
-    TestLoader.load();
-
-    if (autostart) {
-      QUnit.start();
-    }
-  }, 250);
-});
+})();
